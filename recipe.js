ee03c61e6d98fb885046cac2554495de1d46f5d7
Brendan Meehan <bmmeehan3@gmail.com>
2019-05-09 23:24:13 -0400

    revert back to working model

--- recipe.js
+++ recipe.js
@@ -7,6 +7,7 @@ const HEAD = {
     }
 }
 let USERINPUT = [];
+let ingredientCheck = [];
 function handleSearch() {
     $('.find-recipes').click(event => {
         event.preventDefault();
@@ -15,14 +16,23 @@ function handleSearch() {
         $('.protein').val("");
     });
 }
+function handleIngredientSearch() {
+	$('.ingsearch').click(event => {
+		event.preventDefault();
+        emptyResults();
+        getIngredients(USERINPUT);
+        USERINPUT = [];
+		$('.ingredients').val("");
+	});
+}
 function handleAdd() {
     $('.add').click(event => {
         event.preventDefault();
-        USERINPUT.push($('#ingredient-search').val());
-        let test = $('#ingredient-search').val();
-        $('.add-section').append(`<p class="remove">${test}</p>`);
+        let userItem = $('#ingredient-search').val();
+        USERINPUT.push(userItem);
+        ingredientCheck.push(userItem);
+        $('.add-section').append(`<p class="remove js-user-search">${userItem}<button class="remove js-ing">X</button></p>`);
         $('#ingredient-search').val("");
-        console.log(USERINPUT);
     });
 }
 function getRandomRecipe() {
@@ -38,11 +48,26 @@ function getRandomRecipeInformationById(responseJson) {
     .then(response => response.json())
     .then(responseJson => displayRecipeInformation(responseJson))
     .catch(err => {$('.err').text(`Something is wrong: ${err.message}`)});
-
+}
+function getIngredients(input) {
+	let url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=1&ranking=1&ignorePantry=false&ingredients=';
+	let newUrl = url;
+	for (let i = 0; i < input.length; i++) {
+		if (i === input.length - 1) {
+		newUrl = newUrl + input[i];
+		}
+		else {
+		newUrl = newUrl + input[i] + '%2C';
+		}
+    }
+	fetch(newUrl, HEAD)
+	.then(response => response.json())
+    .then(responseJson => getRecipeFromIngredientsById(responseJson))
+    .catch(err => {$('.err').text(`Something went wrong: ${err.message}`)});
 }
 function getRecipeFromIngredientsById(responseJson) {
+    console.log(responseJson);
     let recipeId = responseJson[0].id;
-    checkForPurchased(responseJson);
     let url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${recipeId}/information?includeNutrition=true`;
     fetch(url, HEAD)
     .then(response => response.json())
@@ -54,12 +79,12 @@ function displayRecipeInformation(responseJson) {
     $('.servings').text(`Servings: ${responseJson.servings}`);
     $('.cal').text(`Calories per serving: ${responseJson.nutrition.nutrients[0].amount}`);
     $('.prep').text(`Time: ${responseJson.readyInMinutes} minutes`);
-    $('.ingredients-title').text('Ingredients:');
+    $('.ingredients-title').text(`Ingredients:`);
+    addIngredients(responseJson);
     $('.instructions').append(`${responseJson.instructions}`);
     $('.results').append(`<img class="remove" src="${responseJson.image}" height="100" width="100">`);
 }
 function emptyResults() {
-
     $('.name').empty();
     $('.servings').empty();
     $('.cal').empty();
@@ -69,50 +94,14 @@ function emptyResults() {
     $('.instructions').empty();
     $('.remove').remove();
 }
-function handleIngredientSearch() {
-	$('.ingsearch').click(event => {
-		event.preventDefault();
-		emptyResults();
-        getIngredients(USERINPUT);
-        USERINPUT = [];
-		$('.ingredients').val("");
-	});
-}
-function getIngredients(input) {
-	
-	let url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=1&ranking=1&ignorePantry=false&ingredients=';
-	let newUrl = url;
-	for (let i = 0; i < input.length; i++) {
-		if (i === input.length - 1) {
-		newUrl = newUrl + input[i];
-		}
-		else {
-		newUrl = newUrl + input[i] + '%2C';
-		}
-    }
-    let userArray = input;
-	fetch(newUrl, HEAD)
-	.then(response => response.json())
-    .then(responseJson => getRecipeFromIngredientsById(responseJson, userArray))
-    .catch(err => {$('.err').text(`Something went wrong: ${err.message}`)});
-}
-
-function checkForPurchased(responseJson) {
-    console.log(responseJson);
-    let unused = responseJson[0].unusedIngredients;
-    let used = responseJson[0].usedIngredients;
-    let missing = responseJson[0].missedIngredients;
-    
-    for (let i = 0; i < unused.length; i++) {
-        $('.ingredients').append(`<li class="remove">${unused[i].originalString}</li>`);
-    }
-    for (let i=0; i < used.length; i++) {
-        $('.ingredients').append(`<li class="remove">${used[i].originalString}</li>`);
-    }
-    for (let i = 0; i < missing.length; i++) {
-        $('.ingredients').append(`<li class="remove">${missing[i].originalString}****</li>`);
+function addIngredients(responseJson) {
+    for (let i = 0; i < responseJson.extendedIngredients.length; i++) {
+        $('.ingredients').append(`<li class="remove">${responseJson.extendedIngredients[i].originalString}</li>`);
     }
 }
-$(handleAdd);
-$(handleIngredientSearch);
-$(handleSearch);
+function handleAll() {
+    $(handleAdd);
+    $(handleIngredientSearch);
+    $(handleSearch);
+}
+$(handleAll);
